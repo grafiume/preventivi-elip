44fe26549bc48e6988f9b3236ee88e7f3e5f0db3
grafiume <grafiume@hotmail.it>
2025-10-18 01:07:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.supabaseConfig = {
-  url: 'https://mvvwcsossoqjyqwkpgjm.supabase.co',
-  anon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dndjc29xc29xanlxd2twZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2OTk1MDAsImV4cCI6MjA3NjI3NTUwMH0.phXUlrbGx-f4wIjYAaG8JhTFHcp8utBCWwUvkTQMnKs',
+  url: 'https://<ref-esatto>.supabase.co', // copia & incolla da Project URL
+  anon: '...chiave-anon-che-mi-hai-dato...',
   bucket: 'preventivi-img'
 };
