e809f8d32eda4c853e8c73d58a0da37b4b31f008
grafiume <grafiume@hotmail.it>
2025-10-18 15:26:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-// config.js: inserisci qui la tua init Supabase se non già in app-supabase.js
+window.supabaseConfig={
+  url:'https://mvvwcsoqsoqjyqwkpgjm.supabase.co',
+  anon:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dndjc29xc29xanlxd2twZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2OTk1MDAsImV4cCI6MjA3NjI3NTUwMH0.phXUlrbGx-f4wIjYAaG8JhTFHcp8utBCWwUvkTQMnKs'
+};
