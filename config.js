2f3706c2e1224fa9e87810eed30d5f78eaa0db63
grafiume <grafiume@hotmail.it>
2025-10-18 12:07:31 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-window.supabaseConfig = {
-  url: 'https://mvvwcsoqsoqjyqwkpgjm.supabase.co',
-  anon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dndjc29xc29xanlxd2twZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2OTk1MDAsImV4cCI6MjA3NjI3NTUwMH0.phXUlrbGx-f4wIjYAaG8JhTFHcp8utBCWwUvkTQMnKs',
-  bucket: 'preventivi-img'
+window.supabaseConfig={
+  url:'https://mvvwcsoqsoqjyqwkpgjm.supabase.co',
+  anon:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dndjc29xc29xanlxd2twZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2OTk1MDAsImV4cCI6MjA3NjI3NTUwMH0.phXUlrbGx-f4wIjYAaG8JhTFHcp8utBCWwUvkTQMnKs'
 };
