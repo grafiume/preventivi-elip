46cabc1a1344b9b8b7c975f803890d9bfe489c14
grafiume <grafiume@hotmail.it>
2025-10-26 17:38:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,4 @@ window.preventiviConfig = {
   anon:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dndjc29xc29xanlxd2twZ2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2OTk1MDAsImV4cCI6MjA3NjI3NTUwMH0.phXUlrbGx-f4wIjYAaG8JhTFHcp8utBCWwUvkTQMnKs",
   // (optional) schema/table names if your app reads them
   schema: "public",
-  preventiviTable: "preventivi"
 };
